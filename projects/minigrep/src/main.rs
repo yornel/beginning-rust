eb58abe326dc5104993db0113a327de71d54c79b
YoneTom <yonetomo.asics@gmail.com>
2024-06-14 00:11:18 +0900

    入出力プログラム：Configのコンストラクタを作成する

--- projects/minigrep/src/main.rs
+++ projects/minigrep/src/main.rs
@@ -1,15 +1,9 @@
 use std::env;
-use std::fs::File;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
-
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
-
-    let mut f = File::open(config.filename).expect("file not found");
+    let config = Config::new(&args);
 
     // --snip--
 }
@@ -19,9 +13,13 @@ struct Config {
     filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
+// --snip--
 
-    Config { query, filename }
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Config { query, filename }
+    }
 }
