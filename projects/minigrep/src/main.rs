058bd52fb62067649398aa8648cd374940e562d3
YoneTom <yonetomo.asics@gmail.com>
2024-06-14 00:43:26 +0900

    入出力プログラム：エラー処理（引数数チェック時panic!発生させずnewからResultを返す）

--- projects/minigrep/src/main.rs
+++ projects/minigrep/src/main.rs
@@ -1,9 +1,16 @@
 use std::env;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    //unwrap_or_else：panic!ではない何らか独自のエラー処理を定義できるのです。
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        // 引数解析時に問題
+        println!("Problem parsing arguments: {}", err);
+        //process::exit関数は、 即座にプログラムを停止させ、渡された数字を終了コードとして返します。
+        process::exit(1);
+    });
 
     // --snip--
 }
@@ -16,10 +23,14 @@ struct Config {
 // --snip--
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
