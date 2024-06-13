15bbe73fc9e662d2e2d3b86910ad1e46c4d484f0
YoneTom <yonetomo.asics@gmail.com>
2024-06-13 21:55:52 +0900

    テスト

---
+++ projects/adder/target/debug/deps/adder-e7e499f2563226f1.d
@@ -0,0 +1,7 @@
+/home/yonetani/beginning-rust/projects/adder/target/debug/deps/libadder-e7e499f2563226f1.rmeta: src/lib.rs
+
+/home/yonetani/beginning-rust/projects/adder/target/debug/deps/libadder-e7e499f2563226f1.rlib: src/lib.rs
+
+/home/yonetani/beginning-rust/projects/adder/target/debug/deps/adder-e7e499f2563226f1.d: src/lib.rs
+
+src/lib.rs:
