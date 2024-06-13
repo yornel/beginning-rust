e1f5100316fe317a68da1497aeaaec5dc8839d0c
YoneTom <yonetomo.asics@gmail.com>
2024-06-13 23:27:19 +0900

    入出力プログラム：２つの引数を持つConfig構造体を追加

--- projects/minigrep/src/main.rs
+++ projects/minigrep/src/main.rs
@@ -1,25 +1,27 @@
 use std::env;
 use std::fs::File;
-use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = parse_config(&args);
 
-    println!("Searching for {}", query);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    let mut f = File::open(config.filename).expect("file not found");
+
     // --snip--
-    println!("In file {}", filename);
+}
 
-    // ファイルが見つかりませんでした
-    let mut f = File::open(filename).expect("file not found");
+struct Config {
+    query: String,
+    filename: String,
+}
 
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        // ファイルの読み込み中に問題がありました
-        .expect("something went wrong reading the file");
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
 
-    // テキストは\n{}です
-    println!("With text:\n{}", contents);
+    Config { query, filename }
 }
