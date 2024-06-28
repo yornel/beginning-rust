5a91ad927cc3aa511e6a6d65d8c823934bf53464
YoneTom <yonetomo.asics@gmail.com>
2024-06-28 22:51:15 +0900

    書籍内アプリ：GUIテキストフィールド実装（～p248）

--- projects/guess_word/src/gui.rs
+++ projects/guess_word/src/gui.rs
@@ -1,16 +1,28 @@
 use iced::alignment::Horizontal;
-use iced::{Application, Command, Element, Length, Text};
+use iced::text_input::TextInput;
+use iced::{Application, Column, Command, Element, Length, Text};
 
 #[derive(Debug, Default)]
-struct State;
+struct State {
+    input: iced::text_input::State,
+    input_value: String,
+    announce: String,
+}
+
+#[derive(Debug, Clone)]
+pub enum Message {
+    InputChanged(String),
+    Guess,
+}
 
 pub struct GuessWord {
     state: State,
 }
 
+
 impl Application for GuessWord {
     type Executor = iced::executor::Default;
-    type Message = ();
+    type Message = Message;
     type Flags = ();
 
     fn new(_flags: ()) -> (GuessWord, Command<Self::Message>) {
@@ -26,16 +38,48 @@ impl Application for GuessWord {
         "GuessWord".to_string()
     }
 
-    fn update(&mut self, _message: Self::Message) -> Command<Self::Message> {
+    fn update(&mut self, message: Self::Message) -> Command<Self::Message> {
+        match message {
+            Message::InputChanged(value) => {
+                self.state.input_value = value;
+            }
+            Message::Guess => {
+                self.state.input_value.clear();
+            }
+        };
         Command::none()
     }
 
     fn view(&mut self) -> Element<Self::Message> {
-        Text::new("GuessWord")
+        let title = Text::new("GuessWord")
             .width(Length::Fill)
             .size(60)
-            .color([0.5; 3])
-            .horizontal_alignment(Horizontal::Center)
-            .into()
+            .color([0.5, 0.5, 0.5])
+            .horizontal_alignment(Horizontal::Center);
+
+        let announce = Text::new(&self.state.announce)
+            .width(Length::Fill)
+            .size(20)
+            .color([0.0, 0.4, 1.0])
+            .horizontal_alignment(Horizontal::Center);
+
+        let input = TextInput::new(
+            &mut self.state.input,
+            "input word...",
+            &mut self.state.input_value,
+            Message::InputChanged,
+        )
+        .padding(15)
+        .size(30)
+        .on_submit(Message::Guess);
+
+        let content = Column::new()
+            .padding(40)
+            .max_width(800)
+            .spacing(20)
+            .push(title)
+            .push(announce)
+            .push(input);
+        content.into()
     }
 }
--- projects/guess_word/src/main.rs
+++ projects/guess_word/src/main.rs
@@ -3,6 +3,6 @@ mod gui;   // src/gui.rs の場合は mod gui;
 
 fn main() -> iced::Result {
     let mut settings = Settings::default();
-    settings.window.size = (400, 200);
+    settings.window.size = (400, 600);
     gui::GuessWord::run(settings)            // src/gui.rs の場合は gui::GuessWord::run(settings);
 }
