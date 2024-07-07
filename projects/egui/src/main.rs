fb1c475f635be85eec15a9547aed21b5ad07aa0d
YoneTom <yonetomo.asics@gmail.com>
2024-07-07 22:12:50 +0900

    書籍内アプリ(２冊目)：eguiデスクトップアプリ（スライダー）

--- projects/egui/src/main.rs
+++ projects/egui/src/main.rs
@@ -11,13 +11,13 @@
   enum RadioValue { First, Second, Third }
 
   struct MyEguiApp {
-    pub value:RadioValue,
+    pub value:usize,
   }
   
   impl Default for MyEguiApp {
     fn default() -> MyEguiApp {
       MyEguiApp{
-        value:RadioValue::First,
+        value:0,
       }
     }
   }
@@ -35,23 +35,16 @@
         
         ui.spacing();
   
-        let msg = format!("checked = {:?}.", self.value);
+        let msg = format!("value = {:?}.", self.value);
         let label_txt = egui::RichText::new(msg)
-          .size(32.0);
+          .size(28.0);
         let label = egui::Label::new(label_txt);
         ui.add(label);
   
         ui.separator();
   
-        let label_1 = egui::RichText::new("First").size(24.0);
-        let label_2 = egui::RichText::new("Second").size(24.0);
-        let label_3 = egui::RichText::new("Third").size(24.0);
-        //horizontalで横一列に並べて表示する
-        ui.horizontal(|ui| {
-          ui.radio_value(&mut self.value, RadioValue::First, label_1);
-          ui.radio_value(&mut self.value, RadioValue::Second, label_2);
-          ui.radio_value(&mut self.value, RadioValue::Third, label_3);
-        });
+        let sldr = egui::Slider::new(&mut self.value, 0..=100); //☆
+        ui.add(sldr); //☆
       });
     }
   }
