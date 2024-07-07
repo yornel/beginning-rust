4c5c1a184c2528872071f9549e1676feb39f610f
YoneTom <yonetomo.asics@gmail.com>
2024-07-07 22:28:30 +0900

    書籍内アプリ(２冊目)：eguiデスクトップアプリ（入力イベント）

--- projects/egui/src/main.rs
+++ projects/egui/src/main.rs
@@ -37,7 +37,7 @@
         
         ui.spacing();
   
-        let msg = format!("Title:\"{}\"\nContent:[{}]", self.message, self.content);
+        let msg = format!("input:\"{}\"", self.message);
         let label_txt = egui::RichText::new(msg)
           .font(egui::FontId::proportional(24.0));
         let label = egui::Label::new(label_txt);
@@ -47,7 +47,13 @@
   
         let te_sl = egui::TextEdit::singleline(&mut self.message)
           .font(egui::FontId::proportional(20.0));
-        ui.add(te_sl);
+        let resp = ui.add(te_sl);
+        if resp.changed() {
+          self.content = format!("{}\n{}",
+            self.message.to_uppercase(),
+            self.message.to_lowercase());
+        };
+  
         let te_ml = egui::TextEdit::multiline(&mut self.content)
           .font(egui::FontId::proportional(20.0));
         ui.add(te_ml);
