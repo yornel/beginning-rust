80e8f4d8313a16399c7a3ac3f721b942e1f7e2a0
YoneTom <yonetomo.asics@gmail.com>
2024-07-08 23:07:11 +0900

    axum：クエリパラメーターの取得

--- projects/axum/src/main.rs
+++ projects/axum/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let app = axum::Router::new()
+    .route("/qry", axum::routing::get(handler_query));
+
+  axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
+    .serve(app.into_make_service())
+    .await
+    .unwrap();
+}
+
+async fn handler_query(axum::extract::Query(params): 
+    axum::extract::Query<std::collections::HashMap<String, String>>) -> String {
+  format!("id:{}, name:{}.", params["id"], params["name"])
 }
