c60c449b5568725ad0e848b26148b0f644d2464d
YoneTom <yonetomo.asics@gmail.com>
2024-07-08 23:51:01 +0900

    axum：JSONデータの出力

--- projects/axum/src/main.rs
+++ projects/axum/src/main.rs
@@ -1,7 +1,17 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Mydata {
+  name:String,
+  mail:String,
+  age:u32,
+}
+
+
 #[tokio::main]
 async fn main() {
     let app = axum::Router::new()
-    .route("/qry", axum::routing::get(handler_query));
+    .route("/json/:id", axum::routing::get(handler_json)) ;
 
   axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
     .serve(app.into_make_service())
@@ -9,7 +19,17 @@ async fn main() {
     .unwrap();
 }
 
-async fn handler_query(axum::extract::Query(params): 
-    axum::extract::Query<std::collections::HashMap<String, String>>) -> String {
-  format!("id:{}, name:{}.", params["id"], params["name"])
+async fn handler_json(axum::extract::Path(id):
+    axum::extract::Path<usize>) -> axum::Json<serde_json::Value> {
+  let data:[Mydata;3] = [
+    Mydata {name:String::from("Taro"), 
+      mail:String::from("taro@yamada"), age:39},
+    Mydata {name:String::from("Hanako"), 
+      mail:String::from("hanako@flower"), age:28},
+    Mydata {name:String::from("Sachiko"), 
+      mail:String::from("sachiko@happy"), age:17},
+  ];
+  let item = &data[id];
+  let data = serde_json::json!(item);
+  axum::Json(data)
 }
