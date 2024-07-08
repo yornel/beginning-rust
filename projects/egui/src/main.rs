bb8ad5476a161f254a0e170d1123640330e4b95f
YoneTom <yonetomo.asics@gmail.com>
2024-07-08 18:59:41 +0900

    書籍内アプリ(２冊目)：eguiデスクトップアプリ（四角形描画）

--- projects/egui/src/main.rs
+++ projects/egui/src/main.rs
@@ -1,62 +1,46 @@
-  fn main() {
-    let mut native_options = eframe::NativeOptions::default();
-    native_options.default_theme = eframe::Theme::Light;
-    native_options.initial_window_size = Some(egui::Vec2 {x:400.0, y:200.0});
-  
-    let _ = eframe::run_native("My egui App", native_options,
-      Box::new(|cc| Box::new(MyEguiApp::new(cc))));
-  }
+fn main() {
+  let mut native_options = eframe::NativeOptions::default();
+  native_options.default_theme = eframe::Theme::Light;
+  native_options.initial_window_size = Some(egui::Vec2 {x:400.0, y:300.0});
 
-  #[derive(PartialEq, Debug)]
-  enum MyItem { First, Second, Third }
+  let _ = eframe::run_native("My egui App", native_options,
+    Box::new(|cc| Box::new(MyEguiApp::new(cc))));
+}
 
-  struct MyEguiApp {
-    pub message: String,
-    pub content: String,
-  }
-  
-  impl Default for MyEguiApp {
-    fn default() -> MyEguiApp {
-      MyEguiApp{
-        message: String::from("Hello"),
-        content: String::from("This is content."),
-      }
-    }
-  }
-  
-  impl MyEguiApp {
-    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
-      Self::default()
-    }
-  }
+#[derive(Default)]
+struct MyEguiApp {
+}
 
-  impl eframe::App for MyEguiApp {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-      egui::CentralPanel::default().show(ctx, |ui| {
-        ui.heading("Hello World!");
-        
-        ui.spacing();
-  
-        let msg = format!("input:\"{}\"", self.message);
-        let label_txt = egui::RichText::new(msg)
-          .font(egui::FontId::proportional(24.0));
-        let label = egui::Label::new(label_txt);
-        ui.add(label);
-  
-        ui.separator();
-  
-        let te_sl = egui::TextEdit::singleline(&mut self.message)
-          .font(egui::FontId::proportional(20.0));
-        let resp = ui.add(te_sl);
-        if resp.changed() {
-          self.content = format!("{}\n{}",
-            self.message.to_uppercase(),
-            self.message.to_lowercase());
-        };
-  
-        let te_ml = egui::TextEdit::multiline(&mut self.content)
-          .font(egui::FontId::proportional(20.0));
-        ui.add(te_ml);
-      });
-    }
+impl MyEguiApp {
+  fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+    Self::default()
   }
+}
+
+impl eframe::App for MyEguiApp {
+  fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+    egui::CentralPanel::default().show(ctx, |ui| {
+      ui.heading("Hello World!");
+      plot(ui);
+    });
+  }
+}
+
+fn plot(ui: &mut egui::Ui) {
+  ui.painter().rect_filled(
+    egui::Rect::from_min_max(
+      egui::Pos2::new(50.0, 50.0), 
+      egui::Pos2::new(150.0, 150.0)
+    ), 
+    egui::Rounding::same(20.0), 
+    egui::Color32::RED
+  );
+  ui.painter().rect_stroke(
+    egui::Rect::from_min_max( 
+      egui::Pos2::new(100.0, 100.0), 
+      egui::Pos2::new(200.0, 200.0)
+    ), 
+    egui::Rounding::none(), 
+    egui::Stroke::new(10.0, egui::Color32::GREEN)
+  );
+}
