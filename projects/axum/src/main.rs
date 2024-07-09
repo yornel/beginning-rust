b331aeb3240853b821d3ece88caa86a202dce61b
YoneTom <yonetomo.asics@gmail.com>
2024-07-09 14:01:58 +0900

    axum：フォームの受け取り

--- projects/axum/src/main.rs
+++ projects/axum/src/main.rs
@@ -7,11 +7,17 @@ struct Mydata {
   age:u32,
 }
 
+#[derive(Serialize, Deserialize)]
+struct Myform {
+  name: String,
+  mail: String,
+}
 
 #[tokio::main]
 async fn main() {
     let app = axum::Router::new()
-    .route("/json/:id", axum::routing::get(handler_json)) ;
+    .route("/", axum::routing::get(handle_index))
+    .route("/post", axum::routing::post(handle_post));
 
   axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
     .serve(app.into_make_service())
@@ -19,17 +25,26 @@ async fn main() {
     .unwrap();
 }
 
-async fn handler_json(axum::extract::Path(id):
-    axum::extract::Path<usize>) -> axum::Json<serde_json::Value> {
-  let data:[Mydata;3] = [
-    Mydata {name:String::from("Taro"), 
-      mail:String::from("taro@yamada"), age:39},
-    Mydata {name:String::from("Hanako"), 
-      mail:String::from("hanako@flower"), age:28},
-    Mydata {name:String::from("Sachiko"), 
-      mail:String::from("sachiko@happy"), age:17},
-  ];
-  let item = &data[id];
-  let data = serde_json::json!(item);
-  axum::Json(data)
+async fn handle_index()-> axum::response::Html<String> {
+    let tera = tera::Tera::new("templates/*").unwrap();
+  
+    let mut context = tera::Context::new();
+    context.insert("title", "Index page");
+    context.insert("message", "これはサンプルです。");
+  
+    let output = tera.render("index.html", &context);
+    axum::response::Html(output.unwrap())
+  }
+
+  async fn handle_post(axum::Form(myform): axum::Form<Myform>)
+    -> axum::response::Html<String> {
+  let msg = format!("I am {}<{}>.", myform.name, myform.mail);
+  let tera = tera::Tera::new("templates/*").unwrap();
+
+  let mut context = tera::Context::new();
+  context.insert("title", "Index page");
+  context.insert("message", &msg);
+
+  let output = tera.render("index.html", &context);
+  axum::response::Html(output.unwrap())
 }
